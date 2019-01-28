f1f2945e5948d5ee7efc9ed3043513f6279dfcbc
Yasin Shuman <email@yasin.io>
2019-01-27 19:44:58 -0800

    first commit

---
+++ Project1/Project1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by yasin shuman on 1/27/19.
+//  Copyright © 2019 yasin shuman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,7 @@
+Hacking with Swift Tutorial work
+
+Each Project will be in its own folder
+
+Source Files from author are in sourceFiles/
+
+
